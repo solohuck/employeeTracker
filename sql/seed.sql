2e27fa14089c5995d2556b2bedcd96684a5b1939
solo <solohuckstep@gmail.com>
2022-11-20 23:20:39 -0500

    forgot to add ID to employee seed -- its fixed now

--- sql/seed.sql
+++ sql/seed.sql
@@ -17,12 +17,12 @@ VALUES ("Sales Lead", "1", "Sales", "100000"),
        ("Lawer","8", "Legal", "190000");
     
 INSERT INTO employee (id, first_name, last_name, title, department, salary, manager )
-VALUES ("John","Doe", "Sales Lead", "Sales", "100000", "null"),
-       ("Mike", "Chan", "Salesperson", "Sales", "80000", "John Doe"),
-       ("Ashley", "Rodriguez", "Lead Engineer", "Engineering", "150000", "null"),
-       ("Kevin", "Tupik", "Software Engineer", "Engineering", "120000", "Ashley Rodruguez"),
-       ("Kunal", "Singh", "Account Manager", "Finance", "160000", "null"),
-       ("Malia", "Brown", "Accountant", "Fiance", "125000", "Kunal Singh"),
-       ("Sarah", "Lourd", "Legal Team Lead", "Legal", "250000", "null"),
-       ("Tom", "Allen", "Lawyer", "Legal", "190000", "Sarah Lourd");
+VALUES ("1","John","Doe", "Sales Lead", "Sales", "100000", "null"),
+       ("2","Mike", "Chan", "Salesperson", "Sales", "80000", "John Doe"),
+       ("3","Ashley", "Rodriguez", "Lead Engineer", "Engineering", "150000", "null"),
+       ("4","Kevin", "Tupik", "Software Engineer", "Engineering", "120000", "Ashley Rodruguez"),
+       ("5","Kunal", "Singh", "Account Manager", "Finance", "160000", "null"),
+       ("6","Malia", "Brown", "Accountant", "Fiance", "125000", "Kunal Singh"),
+       ("7","Sarah", "Lourd", "Legal Team Lead", "Legal", "250000", "null"),
+       ("8","Tom", "Allen", "Lawyer", "Legal", "190000", "Sarah Lourd");
        
